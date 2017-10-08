24578458101496af2101a6bf3485b9740c059140
sadiqultra <sadikultra@gmail.com>
2017-10-09 00:36:11 +0600

    comment action

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,6 +1,20 @@
+import * as API from './../utils/api'
+import uuidv1 from 'uuid'
+
+export const RETRIEVE_COMMENT = 'RETRIEVE_COMMENT'
 export const ADD_COMMENT = 'ADD_COMMENT'
 export const DELETE_COMMENT = 'DELETE_COMMENT'
-export const PARENT_DELETED_COMMENT = 'UPDATE_COMMENT'
+export const UPDATE_COMMENT = 'UPDATE_COMMENT'
+
+// create new comment
+export function retrieveComments(json, postId){
+  console.log('comments-actions:', json)
+  return {
+    type: RETRIEVE_COMMENT,
+    comments: json,
+    postId,
+  }
+}
 
 // create new comment
 export function addComment({ comment }){
@@ -18,10 +32,23 @@ export function deleteComment({ comment }){
   }
 }
 
-// parentID deleted
-export function parentDeletedComment({ comment }){
+// update comment
+export function updateComment({ comment }){
   return {
-    type: PARENT_DELETED_COMMENT,
+    type: UPDATE_COMMENT,
     comment,
+  }
+}
+
+// fetch Comments
+
+export function fetchComments(postId) {
+
+  return function (dispatch) {
+
+    return API.fetchComments(postId).then(json =>{
+      console.log('fetchComments==>', json)
+       dispatch(retrieveComments(json, postId) )
+    })
   }
 }
